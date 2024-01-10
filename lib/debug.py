7d8eb8ae0b97cc29e08d2b38aee639dd8e44ebff
KennedyShiku <kenrichshiku@gmail.com>
2024-01-10 10:16:03 +0300

    Completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -1,8 +1,13 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-from config import CONN, CURSOR
-from song import Song
+# from config import CONN, CURSOR
+# from song import Song
 
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+# if __name__ == '__main__':
+#     import ipdb; ipdb.set_trace()
+
+from lib import CONN, CURSOR
+from lib.song import Song
+
+import pytest; pytest.set_trace()
